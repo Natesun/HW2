3ad2cdd0e118a7bbf9a284cff29ca24d187c8e11
Hongbin Sun <hsun4@luc.edu>
2015-06-16 14:44:13 -0500

    I manually calculated pearson test, i will try to make it easier

--- hw2_affordable.py
+++ hw2_affordable.py
@@ -1,5 +1,7 @@
 
-
+# this is a practice test, I learned from Harrison
+# there is a complicated loop to manually get the pearson test, i am trying to
+#some easy way to get this result
 
 def affordableunit():
 #I am trying to make a list with area number and affordable unit numbers
@@ -37,8 +39,35 @@ def status_index():
 status_index()
 
 def income_unitnum():
-    for i in range(6):
-        r, x, y, xy, xx, yy = []
+        values, x, y, xy, xx, yy = ([] for i in range(6)) #create emply list
+    for n in range (0, 77): # for loop to calculate x, y, xy, xx, yy on each row
+        x.append(float(affordableunit())[n][1])
+        y.append(float(status_index())[n][1])
+        xy.append(x[n]*y[n])
+        xx.append(x[n]**2)
+        yy.append(y[n]**2)
+        values.append(x[n], y[n], xy[n], xx[n], yy[n])
+    return values
+income_unitnum()
+
+def correlation(pearsonlist):
+    sx, sy, sxy, sx2, sy2 = [0]*5
+    n = len(pearsonlist)
+    for row in pearsonlist:
+        sx += row[0]
+        sy += row[1]
+        sxy += row[2]
+        sx2 += row[3]
+        sy2 += row[4]
+    r = (sxy-sx*sy)/ pow((sx2-pow(sx,2)/n)*(sy2-sy**2/n)), 0.5)
+    return r
+
+def result():
+    print("the pearson correlation r between affordable housing unit numbers/n"
+          "and per capital income is " , correlation(income_unitnum()))
+
+
+
 
 
 
