4c4ab31e18f0223a400c4ce6776188e6fc85044d
Hongbin Sun <hsun4@luc.edu>
2015-06-16 22:20:05 -0500

    Please take a look of this data, forgive my poor programming skills

---
+++ pearsonr.py
@@ -0,0 +1,56 @@
+__author__ = 'HongbinSun'
+# after parsing from each data sets, get two lists incomelist[]
+# and unitnumlist[] with the same order and same length (manually transformed the data)
+#now import these two lists and get the r value
+__author__ = 'HongbinSun'
+def incomelist(): #I am making a incomelist in the area number order of 77 chicago
+    #area to store the capital income, I will compare this with unitnumlist in pearson
+    import csv
+
+    with open("status.csv") as csvfile:
+        readstatus = csv.reader(csvfile, delimiter=",")
+        income = []
+        rownum = 0
+        for row in readstatus:
+            if rownum == 0:
+                pass
+            elif rownum ==77:
+                pass
+            else:
+                income.append(float(row[7]))
+            rownum +=1
+        return(income)  #gosh I finally know the difference between print and return
+
+incomelist()
+
+def unitnumlist(): #I am making a unitnumlist in the area number order of 77 chicago
+    #area to store the affordable housing unit numbers(I admit that I manually
+    # transformed a lot, but for now, let us just let it be), I will compare this with
+    # incomelist in pearson
+    import csv
+
+    with open("affordable2.csv") as csvfile:
+        readstatus = csv.reader(csvfile, delimiter=",")
+        unitnum = []
+        rownum = 0
+        for row in readstatus:
+            if rownum == 0:
+                pass
+            else:
+                unitnum.append(float(row[8]))
+            rownum +=1
+        return(unitnum)
+
+unitnumlist()
+x = incomelist()
+y = unitnumlist()
+print(x)
+print(len(x))
+print(y)
+print(len(y))
+
+from pydoc import help
+from scipy.stats.stats import pearsonr
+print("the r and p values for pearson's correlation is" , pearsonr(x,y))
+
+
