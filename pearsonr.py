787443824daed59e55a26f05f9deec98929b7bbe
Hongbin Sun <hsun4@luc.edu>
2015-06-17 13:41:35 -0500

    OMG, I just did my first unit test

--- pearsonr.py
+++ pearsonr.py
@@ -53,17 +53,19 @@ def pearsontest():
 
     from pydoc import help
     from scipy.stats.stats import pearsonr
-    return("the r and p values for pearson's correlation are" , pearsonr(x,y))
+    print("the r and p values for pearson's correlation are" , pearsonr(x,y))
 pearsontest()
 
 import unittest
-from incomelist import incomelist
-class Test(unittest, TestCase):
-     def test_incomelist(self):
-         self.assertEquals(len(incomelist()), 77)
 
-if __name__ == "__main__"
-    unitest.main()
+class Testpr(unittest.TestCase):
+     def test_basic_equal(self):
+         self.assertEqual(len(incomelist()), 77)
+         self.assertEqual(len(unitnumlist()), 77)
+
+if __name__ == '__main__':
+    unittest.main()
+
 
 
 
